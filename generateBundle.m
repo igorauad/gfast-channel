6865b6e791ce00cac386b6fa04bba676ed5e274a
Igor Freire <igor.auad@gmail.com>
2015-08-11 14:17:29 -0300

    Allow oversampling in the model for windowing
    When oversample is applied, the fs in which the channel response is computed is increased to create a margin for application of a non-rectangular window. Currently a raised-cosine window is used.

--- generateBundle.m
+++ generateBundle.m
@@ -1,19 +1,28 @@
-clear all
+clear all, close all
 clc
 
 set(0,'DefaultTextInterpreter','LaTex')
 set(0,'DefaultAxesFontSize',11)
 
 %% Parameters
-filename = 'gfast_bundle_212mhz';
+filename    = 'gfast_bundle_212mhz_oversampledBy2';
+oversample  = 2;            % 1 if not using, > 1 for using
+bandwidth   = 105.984e6;
+Nfft        = 4096;
+Zl          = 100;
+Zso         = 100;
+
 
 Params.shapingFunction = 1;  % Shaping function for TNO model:
                                 %     0: 'rat'
                                 %     1: 'sqrt_rat'
-Params.Nfft      = 2*4096;         % FFT size
-Params.bandwidth = 2*105.984e6;    % Bandwidth
-Params.Zl        = 100;            % Load impedance
-Params.Zso       = 100;            % Source impedance
+
+%% Derived Parametes
+
+Params.Nfft      = oversample*2*Nfft;         % FFT size
+Params.bandwidth = oversample*2*bandwidth;    % Bandwidth
+Params.Zl        = Zl;                        % Load impedance
+Params.Zso       = Zso;                       % Source impedance
 
 topologies = { 'D1-H1',... %1
                'D2-H2',... %2
@@ -24,8 +33,6 @@ topologies = { 'D1-H1',... %1
                'D6-H6',... %7
                'D6-none'}; %8
 
-%% Derived Parametes
-
 bandwidth = Params.bandwidth;
 Nfft = Params.Nfft;
 fs = 2 * bandwidth;
@@ -33,6 +40,16 @@ nTones = Params.Nfft/2 + 1;
 nLines = length(topologies);
 toneSpacing = fs / Nfft;
 
+if(oversample > 1)
+    D = fdesign.pulseshaping(oversample,'Raised Cosine', ...
+        'Nsym,Beta', Nfft, 0.2);
+    Hwindow = design(D); % H is a DFILT
+    [Hd_window, W] = freqz(Hwindow, Nfft, 'whole');
+    figure, plot(W/(2*pi), 20*log10(abs(Hd_window)))
+    title('Frequency-domain Window');
+    ylabel('Magnitude Response (dB)')
+    xlabel('Digital Frequency (f / fs)')
+end
 
 %% Calculate equivalent ABCD
 
@@ -47,9 +64,38 @@ for iLine = 1:nLines
     h = cirFromABCD( ABCD, Params );
     % Frequency response:
     H_freq = fft(h, Nfft);
+
+    % If oversample is adopted, apply a non-rectangular window in freq.
+    if(oversample > 1)
+        figure
+        plot((0:(Nfft-1))*toneSpacing*1e-6, 10*log10(abs(H_freq)))
+
+        % Note: standard IFFT uses rectangular window, which may induce
+        % ripples in the resulting impulse response.
+        w = abs(Hd_window);
+
+        % Apply window
+        H_freq = H_freq .* w;
+
+        hold on
+        plot((0:(Nfft-1))*toneSpacing*1e-6, 10*log10(abs(H_freq)), 'r')
+        legend('Original', 'Windowed')
+        xlabel('Freq (MHz)')
+    end
+
     H(:, iLine, iLine) = H_freq(1:nTones, 1);
+
     figure
     plot(h)
+
+    if(oversample > 1)
+        % New (windowed) response
+        h = ifft(H_freq, Nfft);
+
+        hold on
+        plot(h, 'r')
+        legend('Original', 'Windowed')
+    end
 end
 
 %% Export channel struct
